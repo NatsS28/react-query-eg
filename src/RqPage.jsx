81f4642237701f233c6919e59a98ccf75fb6d83b
nadarajan.s <nadarajan.s@gofrugal.com>
2022-06-16 11:33:08 +0530

    [SN] commit for error handling comparison in traditional vs react query

--- src/RqPage.jsx
+++ src/RqPage.jsx
@@ -3,12 +3,16 @@ import axios from 'axios'
 import { useQuery } from 'react-query'
 function RqPage() {
 
-    const { isLoading, data } = useQuery('super-heros', () => {
-        return axios.get('http://localhost:3000/superheroes');
+    const { isLoading, data, isError,error } = useQuery('super-heros', () => {
+        return axios.get('http://localhost:3000/superheroes1');
     })
 
     if (isLoading) {
         return <h2>Loading...</h2>
+    }
+
+    if (isError) {
+        return <h2>{error.message}</h2>
     }
   return (
       <>
